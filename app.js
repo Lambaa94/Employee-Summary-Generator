5655ef5ae649eada95ff57d1d773f5fb9b813555
James Lamb <jameslamb13@gmail.com>
2020-09-26 01:17:48 -0400

    all tests are running to 100% and wrote out all questions that have to be asked to build team.

--- app.js
+++ app.js
@@ -11,69 +11,109 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const Choices = require("inquirer/lib/objects/choices");
 
-// There needs to be a for loop
-// Starts with manager question then asks for list
-// 3 things are the same and you can use an object and prototype to add on ???
+const employees = []
 
+console.log("Please build your team.")
+inquirer.prompt([
+    {
+        type: "input",      
+        message: "What is your manager's name?",
+        name: "managerName"
+    },
+    {
+        type: "input",       
+        message: "What is your manager's id?",
+        name: "managerId"
+    },
+    {
+        type: "input",      
+        message: "What is your manager's email?",
+        name: "managerEmail"
+    },
+    {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "office"
 
+    }]).then(function (managerAnswers) {
+        const manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.office)
+        employees.push(manager);
+        
+        makeEmployees();
+        
+    });
+        
+    async function makeEmployees() {
+    await inquirer.prompt([
+            {
+                type: "list",
+                message: "Which type of team member would you like to add?",
+                name: "addMember",
+                choices: ["Engineer", "Intern", "I don't want to add more members."]
+            }]).then(function (addMember) {
+                if (addMember === Engineer) {
+                    inquirer.prompt([{
+                        type: "input",
+                        message: "What is your engineer's name?",
+                        name: "engineerName"
+                    },
+                    {
+                        type: "input",
+                        message: "What is your engineer's id",
+                        name: "engineerId"
+                    },
+                    {
+                        type: "input",
+                        message: "What is your engineer's email?",
+                        name: "engineersEmail"
+                    },
+                    {
+                        type: "input",
+                        message: "What is your engineer's GitHub?",
+                        name: "gitHub"
+                    }]).then(function (engineerAnswers) {
+                        const engineer = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineersEmail, engineerAnswers.github)
+                        employees.push(engineer);
+                        makeEmployees();
+                    
+                    })
+                } else if (addMember === Intern) {
+                    inquirer.prompt([{
+                        type: "input",
+                        message: "What is your intern's name?",
+                        name: "internName"
+                    },
+                    {
+                        type: "input",
+                        message: "What is your intern's id",
+                        name: "internId"
+                    },
+                    {
+                        type: "input",
+                        message: "What is your intern's email?",
+                        name: "internsEmail"
+                    },
+                    {
+                        type: "input",
+                        message: "Where did your intern go to school?",
+                        name: "school"
+                    }]).then(function (internAnswers) {
+                        const intern = new Intern(internAnswers.internName, internAnswers.internId, internAnswers.internEmail, internAnswers.office)
+                        employees.push(intern);
+                        makeEmployees();
+                    
+                    })
+                } else { return console.log("Generating employees...")
+                }
+            })};
+   
 
-// let Questions() = [{ 
-//     type: "input",      // { name }
-//     message: "What is your manager's name?",
-//     name: "manager"
-// },
-// {
-//     type: "input",       // { id }
-//     message: "What is your manager's id?",
-//     name: "managerId"
-// },
-// {
-//     type: "input",      // { email }
-//     message: "What is your manager's email?",
-//     name: "managerEmail"
-// },
-// {
-//     type: "input",
-//     message: "What is your manager's office number?",
-//     name: "office"
-// },
-// {
-//     type: "list",
-//     message: "Which type of team member would you like to add?",
-//     name: "addMember",
-//     choices: ["Engineer", "Intern", "I don't want to add more members."]
-// },
+const html = render(employees);
 
-// // if engineer then ->
-// // Probably only need last question. same goes with intern.
-// {
-//     type: "input",
-//     message: "What is your engineer's name?",
-//     name: "engineerName"
-// },
-// {
-//     type: "input",
-//     message: "What is your engineer's id",
-//     name: "engineerId"
-// },
-// {   
-//     type: "input",      
-//     message: "What is your engineer's email?",
-//     name: "engineersEmail"
-// },
-// {
-//     type: "input",
-//     message: "What is your engineer's GitHub?",
-//     name: "gitHub"
-// },
-
-// ]
-
-
-
-// if done then render()
-// fs.writeFile with blocks of html
-
+fs.writeFile(outputPath, html, function (err) {
+    if (err) throw err;
+    console.log("Success!")
+});
 
 
 
