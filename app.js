d3fff61e9405466a186a078147ef3d4e39f08861
James Lamb <jameslamb13@gmail.com>
2020-09-27 00:45:36 -0400

    added get name email and id to all employees.

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ const employees = []
 console.log("Please build your team.")
 inquirer.prompt([
     {
-        type: "input",      
+        type: "input",
         message: "What is your manager's name?",
         name: "managerName"
     },
     {
-        type: "input",       
+        type: "input",
         message: "What is your manager's id?",
         name: "managerId"
     },
     {
-        type: "input",      
+        type: "input",
         message: "What is your manager's email?",
         name: "managerEmail"
     },
@@ -36,84 +36,92 @@ inquirer.prompt([
         name: "office"
 
     }]).then(function (managerAnswers) {
-        const manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.office)
+        const manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.office);
         employees.push(manager);
-        
+
         makeEmployees();
-        
+
     });
-        
-    async function makeEmployees() {
-    await inquirer.prompt([
-            {
-                type: "list",
-                message: "Which type of team member would you like to add?",
-                name: "addMember",
-                choices: ["Engineer", "Intern", "I don't want to add more members."]
-            }]).then(function (addMember) {
-                if (addMember === Engineer) {
-                    inquirer.prompt([{
-                        type: "input",
-                        message: "What is your engineer's name?",
-                        name: "engineerName"
-                    },
-                    {
-                        type: "input",
-                        message: "What is your engineer's id",
-                        name: "engineerId"
-                    },
-                    {
-                        type: "input",
-                        message: "What is your engineer's email?",
-                        name: "engineersEmail"
-                    },
-                    {
-                        type: "input",
-                        message: "What is your engineer's GitHub?",
-                        name: "gitHub"
-                    }]).then(function (engineerAnswers) {
-                        const engineer = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineersEmail, engineerAnswers.github)
-                        employees.push(engineer);
-                        makeEmployees();
-                    
-                    })
-                } else if (addMember === Intern) {
-                    inquirer.prompt([{
-                        type: "input",
-                        message: "What is your intern's name?",
-                        name: "internName"
-                    },
-                    {
-                        type: "input",
-                        message: "What is your intern's id",
-                        name: "internId"
-                    },
-                    {
-                        type: "input",
-                        message: "What is your intern's email?",
-                        name: "internsEmail"
-                    },
-                    {
-                        type: "input",
-                        message: "Where did your intern go to school?",
-                        name: "school"
-                    }]).then(function (internAnswers) {
-                        const intern = new Intern(internAnswers.internName, internAnswers.internId, internAnswers.internEmail, internAnswers.office)
-                        employees.push(intern);
-                        makeEmployees();
-                    
-                    })
-                } else { return console.log("Generating employees...")
-                }
-            })};
-   
 
-const html = render(employees);
+function makeEmployees() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Which type of team member would you like to add?",
+            name: "addMember",
+            choices: ["Engineer", "Intern", "I don't want to add more members."],
+        }]).then(function(addMember) {
+            if (addMember === "Engineer") {
+                inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your engineer's name?",
+                    name: "engineerName"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's id",
+                    name: "engineerId"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's email?",
+                    name: "engineersEmail"
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's GitHub?",
+                    name: "gitHub"
+                },
+                ]).then(function (engineerAnswers) {
+                    const engineer = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerId, engineerAnswers.engineersEmail, engineerAnswers.github)
+                    employees.push(engineer);
+                    makeEmployees();
 
-fs.writeFile(outputPath, html, function (err) {
-    if (err) throw err;
-    console.log("Success!")
-});
+                })
+            } else if (addMember === "Intern") {
+                inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your intern's name?",
+                    name: "internName"
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's id",
+                    name: "internId"
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's email?",
+                    name: "internsEmail"
+                },
+                {
+                    type: "input",
+                    message: "Where did your intern go to school?",
+                    name: "school"
+                },
+                ]).then(function (internAnswers) {
+                    const intern = new Intern(internAnswers.internName, internAnswers.internId, internAnswers.internEmail, internAnswers.office)
+                    employees.push(intern);
+                    makeEmployees();
+
+                })
+            } else {
+                console.log("Generating employees...")
+               
+                const html = render(employees);
+
+                fs.writeFile(outputPath, html, function (err) {
+                    if (err) throw err;
+                    console.log("Success!")
+                });
+            }
+
+
+
+        })};
+
 
 
 
